b693b4ddcf23f5c71135974b997399ca36ce6b8b
Joshua <psyjr4@nottingham.ac.uk>
2020-08-13 22:07:13 +0100

    Added a serverless function for API gateway

--- AdventuresOfWilbur/test/AdventuresOfWilbur.Tests/FunctionTest.cs
+++ AdventuresOfWilbur/test/AdventuresOfWilbur.Tests/FunctionTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.APIGatewayEvents;
 
 using AdventuresOfWilbur;
 
@@ -13,16 +14,25 @@ namespace AdventuresOfWilbur.Tests
 {
     public class FunctionTest
     {
-        [Fact]
-        public void TestToUpperFunction()
+        public FunctionTest()
         {
+        }
 
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+        [Fact]
+        public void TetGetMethod()
+        {
+            TestLambdaContext context;
+            APIGatewayProxyRequest request;
+            APIGatewayProxyResponse response;
 
-            Assert.Equal("HELLO WORLD", upperCase);
+            Functions functions = new Functions();
+
+
+            request = new APIGatewayProxyRequest();
+            context = new TestLambdaContext();
+            response = functions.Get(request, context);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal("Hello AWS Serverless", response.Body);
         }
     }
 }
