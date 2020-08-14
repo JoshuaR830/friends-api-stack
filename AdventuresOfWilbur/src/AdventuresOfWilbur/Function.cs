0c1603a461ef0f31d447a22a6c219c90b9834156
Joshua <psyjr4@nottingham.ac.uk>
2020-08-14 08:54:57 +0100

    Updated template to add all of the resources for api gateway and fixed lambda response

--- AdventuresOfWilbur/src/AdventuresOfWilbur/Function.cs
+++ AdventuresOfWilbur/src/AdventuresOfWilbur/Function.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -20,11 +20,19 @@ namespace AdventuresOfWilbur
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandlerAsync(object input, ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandlerAsync(APIGatewayProxyRequest input, ILambdaContext context)
         {
             Console.WriteLine(input);
             var imageName = "WP_20160601_20_39_24_Pro.jpg";
-            return $"BucketBaseUrl/{imageName}";
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+                Body = $"BucketBaseUrl/{imageName}"
+            };
+
+            return response;
         }
     }
 }
