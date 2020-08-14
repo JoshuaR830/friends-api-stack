86b5a1987c8935cdc7b462b105c465b6ddea571f
Joshua <psyjr4@nottingham.ac.uk>
2020-08-14 10:30:25 +0100

    Added DynamoDB request

--- AdventuresOfWilbur/src/AdventuresOfWilbur/Handler.cs
+++ AdventuresOfWilbur/src/AdventuresOfWilbur/Handler.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 
 namespace AdventuresOfWilbur
 {
     public class Handler
     {
+        private readonly IAmazonDynamoDB _dynamoDb;
+        
         private const string BucketBaseUrl = "https://adventures-of-wilbur-images.s3.eu-west-2.amazonaws.com";
+
+        public Handler(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
@@ -21,7 +30,53 @@ namespace AdventuresOfWilbur
             {
                 var storyItem = int.Parse(input.QueryStringParameters["storyItemNumber"]);
                 Console.WriteLine(storyItem);
+                
+                var tableDescription = new DescribeTableRequest
+                {
+                    TableName = "AdventuresOfWilburImageTable"
+                };
 
+                var description = await _dynamoDb.DescribeTableAsync(tableDescription);
+                var numberOfItems = description.Table.ItemCount;
+                Console.WriteLine($"Item count: {numberOfItems}");
+                
+                var getRequest = new GetItemRequest
+                {
+                    TableName = "AdventuresOfWilburImageTable",
+                    Key = new Dictionary<string, AttributeValue>
+                    {
+                        {"ImageId", new AttributeValue{ N = storyItem.ToString() }}
+                    },
+                    ProjectionExpression = "#title, #description, #imageKey, #friends",
+                    ExpressionAttributeNames = new Dictionary<string, string>
+                    {
+                        {"#imageKey", "ImageKey"},
+                        {"#title", "Title"},
+                        {"#description", "Description"},
+                        {"#friends", "Friends"}
+                    },
+                };
+
+                var response = await _dynamoDb.GetItemAsync(getRequest);
+                
+                if(!response.IsItemSet)
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 403,
+                        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+                        Body = ""
+                    };
+
+                var imageKey = response.Item["ImageKey"].S;
+                var imageTitle = response.Item["Title"].S;
+                var imageDescription = response.Item["Description"].S;
+                var imageFriends = response.Item["Friends"].SS;
+
+                Console.WriteLine(imageKey);
+                Console.WriteLine(imageTitle);
+                Console.WriteLine(imageDescription);
+                Console.WriteLine(imageFriends);
+                
                 var imageName = "";
                 if (storyItem == 1)
                 {
