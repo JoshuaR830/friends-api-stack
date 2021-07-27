0f243520846fc65b6b20525d5ba46d6dbf5945fd
Joshua <joshuaquarryhouse@gmail.com>
2021-07-27 11:48:55 +0100

    Added base for the lambda

---
+++ BotuaFriendTime/src/BotuaFriendTime/Function.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace BotuaFriendTime
+{
+    public class Function
+    {
+        private ServiceCollection _serviceCollection;
+
+        public Function()
+        {
+            ConfigureServices();
+        }
+        
+        private void ConfigureServices()
+        {
+            _serviceCollection = new ServiceCollection();
+            _serviceCollection.AddDefaultAWSOptions(new AWSOptions());
+            _serviceCollection.AddAWSService<IAmazonDynamoDB>();
+            _serviceCollection.AddTransient<Handler>();
+        }
+
+        public async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            using (ServiceProvider serviceProvider = _serviceCollection.BuildServiceProvider())
+                return await serviceProvider.GetService<Handler>().Handle(input);
+        }
+    }
+}
---
+++ BotuaFriendTime/src/BotuaFriendTime/Handler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.Lambda.APIGatewayEvents;
+
+namespace BotuaFriendTime
+{
+    public class Handler
+    {
+        private readonly IAmazonDynamoDB _dynamoDb;
+
+        
+        public Handler(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> Handle (APIGatewayProxyRequest input)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+                Body = input?.Body?.ToUpper()
+            };
+        }
+    }
+}
