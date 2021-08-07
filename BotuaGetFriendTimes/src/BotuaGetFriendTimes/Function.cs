dbde2b137ba9fd0b86aebadbe8b417107f348dab
Joshua <joshuaquarryhouse@gmail.com>
2021-08-07 10:41:33 +0100

    Added more template code

--- BotuaGetFriendTimes/src/BotuaGetFriendTimes/Function.cs
+++ BotuaGetFriendTimes/src/BotuaGetFriendTimes/Function.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Amazon.DynamoDBv2;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -12,6 +15,21 @@ namespace BotuaGetFriendTimes
 {
     public class Function
     {
+
+        private ServiceCollection _serviceCollection;
+
+        public Function()
+        {
+            ConfigureServices();
+        }
+
+        private void ConfigureServices()
+        {
+            _serviceCollection = new ServiceCollection();
+            _serviceCollection.AddDefaultAWSOptions(new AWSOptions());
+            _serviceCollection.AddAWSService<IAmazonDynamoDB>();
+            _serviceCollection.AddTransient<Handler>();
+        }
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
@@ -19,9 +37,10 @@ namespace BotuaGetFriendTimes
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            using (ServiceProvider serviceProvider = _serviceCollection.BuildServiceProvider())
+                return await serviceProvider.GetService<Handler>().Handle(input);
         }
     }
 }
---
+++ BotuaGetFriendTimes/src/BotuaGetFriendTimes/Handler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.Lambda.APIGatewayEvents;
+
+namespace BotuaGetFriendTimes
+{
+    public class Handler
+    {
+        private readonly IAmazonDynamoDB _dynamoDb;
+    
+        public Handler(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
+        
+        public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest input)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> {{"Content-Type", "application/json"}},
+                Body = "Time"
+            };
+        }
+    }
+}
