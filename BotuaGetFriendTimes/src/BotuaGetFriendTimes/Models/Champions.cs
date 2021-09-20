06eaf99ae71b6d4fc885b94563a0353c7fc0cbe6
Joshua <joshuaquarryhouse@gmail.com>
2021-09-20 21:04:33 +0100

    Changed the property names

--- BotuaGetFriendTimes/src/BotuaGetFriendTimes/Models/Champions.cs
+++ BotuaGetFriendTimes/src/BotuaGetFriendTimes/Models/Champions.cs
@@ -1,12 +1,25 @@
-﻿namespace BotuaGetFriendTimes.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BotuaGetFriendTimes.Models
 {
     public class Champions
     {
+        [JsonPropertyName("active")]
         public Champion ActiveChampion { get; set; }
+        
+        [JsonPropertyName("muted")]
         public Champion Muted { get; set; }
+        
+        [JsonPropertyName("deafened")]
         public Champion Deafened { get; set; }
+        
+        [JsonPropertyName("afk")]
         public Champion Afk { get; set; }
+        
+        [JsonPropertyName("streaming")]
         public Champion Streaming { get; set; }
+        
+        [JsonPropertyName("video")]
         public Champion Video { get; set; }
         
         public Champions(Champion activeChampion, Champion mutedChampion, Champion deafenedChampion, Champion afkChampion, Champion streamingChampion, Champion videoChampion)
